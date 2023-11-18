bc5856fd2552d0278210d9b777727dcc686e94f7
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-18 22:10:51 +0000

    Switch back to GNOME
    This seems to screw up FFXIV the least (https://unix.stackexchange.com/questions/761164/pressing-ctrl-or-shift-does-nothing is still pending)

--- configuration.nix
+++ configuration.nix
@@ -45,8 +45,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
