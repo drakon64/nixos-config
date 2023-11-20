ab1b35cdfd0393dc6b9e37d1190b5ada96b0de03
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-20 19:16:23 +0000

    Fix setting kernel version and disable ZFS

--- live.nix
+++ live.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   boot.supportedFilesystems = [ "bcachefs" ];
 
   hardware.nvidia = {
@@ -13,6 +13,11 @@
   };
 
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [(final: super: {
+    zfs = super.zfs.overrideAttrs(_: {
+      meta.platforms = [];
+    });
+  })];
 
   services.xserver.videoDrivers = [ "nvidia" ];
 }
