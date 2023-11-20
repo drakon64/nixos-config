cf538aa89ae711ffa72b723a82eb011f2c37c479
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-20 19:55:56 +0000

    Use default kernel

--- live.nix
+++ live.nix
@@ -4,20 +4,12 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
   ];
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   boot.supportedFilesystems = [ "bcachefs" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;
   };
-
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [(final: super: {
-    zfs = super.zfs.overrideAttrs(_: {
-      meta.platforms = [];
-    });
-  })];
 
   services.xserver.videoDrivers = [ "nvidia" ];
 }
