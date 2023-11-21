82488a6ea7acb27b3ca93eeb2ae0051cb8444950
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-21 20:09:54 +0000

    Use latest kernel, bcachefs, and open NVIDIA drivers

--- live.nix
+++ live.nix
@@ -4,12 +4,20 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
   ];
 
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   boot.supportedFilesystems = [ "bcachefs" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;
   };
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [(final: super: {
+    zfs = super.zfs.overrideAttrs(_: {
+      meta.platforms = [];
+    });
+  })];
 
   services.xserver.videoDrivers = [ "nvidia" ];
 }
