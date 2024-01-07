d43ff4cc688cc80b26210abbc25f68b8ef0302a8
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-01-07 14:01:47 +0000

    Build a Raspberry Pi image

---
+++ rpi/configuration.nix
@@ -0,0 +1,28 @@
+{ config, inputs, lib, pkgs, ... }:
+
+let
+  sys = inputs.nixos.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      ({ config, pkgs, lib, modulesPath, ... }: {
+        imports = [ (modulesPath + "/installer/netboot/netboot-minimal.nix") ];
+      })
+    ];
+  };
+
+  build = sys.config.system.build;
+in {
+  boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+
+  services.pixiecore = {
+    enable = true;
+    openFirewall = true;
+    dhcpNoBind = true;
+
+    mode = "boot";
+    kernel = "${build.kernel}/bzImage";
+    initrd = "${build.netbootRamdisk}/initrd";
+  };
+  
+  system = "aarch64-linux";
+}
