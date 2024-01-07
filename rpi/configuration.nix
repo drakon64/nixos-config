8b2c4b79ac35dd6877cd19a185f362033c4f8007
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-01-07 16:44:46 +0000

    Disable nested netboot image again

--- rpi/configuration.nix
+++ rpi/configuration.nix
@@ -1,24 +1,10 @@
 { config, pkgs, ... }:
 
-let
-  nixpkgs = builtins.getFlake "github:nixos/nixpkgs/nixos-23.11";
-
-  netboot = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-
-    modules = [
-      ({ config, pkgs, ... }: {
-        imports = [ <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix> ];
-
-        system.stateVersion = "23.11";
-      })
-    ];
-  }.config.system.build;
-in {
+{
   imports = [ <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix> ];
 
   boot = {
-    binfmt.emulatedSystems = [ "x86_64-linux" ];
+    # binfmt.emulatedSystems = [ "x86_64-linux" ];
     kernelPackages = pkgs.linuxPackages_latest;
   };
 
@@ -29,8 +15,8 @@ in {
     mode = "boot";
     openFirewall = true;
 
-    kernel = "${netboot.kernel}/bzImage";
-    initrd = "${netboot.netbootRamdisk}/initrd";
+    # kernel = "${netboot.kernel}/bzImage";
+    # initrd = "${netboot.netbootRamdisk}/initrd";
   };
 
   networking = {
