d6deaec8ffe7e4e65daf23528adecdf166d9dff1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-01-07 14:30:34 +0000

    Use the latest Linux kernel

--- rpi/configuration.nix
+++ rpi/configuration.nix
@@ -5,7 +5,10 @@
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
   ];
 
-  boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+  boot = {
+    binfmt.emulatedSystems = [ "x86_64-linux" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+  }
 
   system.stateVersion = "23.11";
 }
