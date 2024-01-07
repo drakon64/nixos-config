ea8ed7ea04f836af1dcc0731bfab83d9a07c25ee
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-01-07 14:11:51 +0000

    Fix importing base SD image configuration

--- rpi/configuration.nix
+++ rpi/configuration.nix
@@ -12,7 +12,9 @@ let
 
   build = sys.config.system.build;
 in {
-  imports = [ "nixos/modules/installer/sd-card/sd-image-aarch64.nix" ];
+  imports = [
+    <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+  ];
 
   boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
 
