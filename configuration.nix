d254c95bbae2ade495377285a92a9cc9ac5eb9ea
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-01-08 19:06:20 +0000

    Use latest Linux kernel

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "nixos";
 
   networking.networkmanager.enable = true;
