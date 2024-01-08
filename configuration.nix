c7d3c980e050297f3164353fa60a703a7754740c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-01-08 19:49:44 +0000

    Remove explicit `bcachefs-tools` requirement

--- configuration.nix
+++ configuration.nix
@@ -67,7 +67,6 @@
 
   environment.systemPackages = with pkgs; [
     _1password-gui
-    bcachefs-tools
     vim
     xivlauncher
 
--- live.nix
+++ live.nix
@@ -10,7 +10,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    bcachefs-tools
     vim
   ];
 
