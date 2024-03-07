4324f795a54d44b75a46ac1d28aa3c5ecb827237
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-03-07 22:07:32 +0000

    Add laptop image

---
+++ laptop.nix
@@ -0,0 +1,135 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      efi.canTouchEfiVariables = true;
+
+      systemd-boot.enable = true;
+    };
+
+    supportedFilesystems = [ "bcachefs" ];
+  };
+
+  console.keyMap = "uk";
+
+  documentation.nixos.enable = false;
+
+  environment = {
+    gnome.excludePackages = with pkgs; [
+      epiphany
+      gnome.geary
+      gnome.gnome-calendar
+      gnome.gnome-characters
+      gnome.gnome-clocks
+      gnome.gnome-contacts
+      gnome.gnome-font-viewer
+      gnome.gnome-maps
+      gnome.gnome-music
+      gnome.simple-scan
+      gnome-connections
+      gnome-tour
+      gnome.yelp
+      snapshot
+    ];
+
+    systemPackages = with pkgs; [
+      firefox
+      jetbrains.idea-ultimate
+      microsoft-edge
+      vim
+
+      gnomeExtensions.alphabetical-app-grid
+      gnomeExtensions.appindicator
+      gnomeExtensions.dash-to-dock
+    ];
+  };
+
+  hardware = {
+    nvidia.modesetting.enable = true;
+
+    pulseaudio.enable = false;
+  };
+
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+  };
+
+  networking = {
+    hostName = "nixos";
+
+    networkmanager.enable = true;
+  };
+
+  nixpkgs = {
+    config.allowUnfree = true;
+
+    overlays = [(final: super: {
+      zfs = super.zfs.overrideAttrs(_: {
+        meta.platforms = [];
+      });
+    })];
+  };
+
+  services = {
+    openssh.enable = false;
+
+    pipewire = {
+      enable = true;
+
+      alsa = {
+        enable = true;
+      };
+
+      pulse.enable = true;
+    };
+
+    xserver = {
+      enable = true;
+
+      excludePackages = [ pkgs.xterm ];
+
+      displayManager.gdm.enable = true;
+
+      desktopManager.gnome.enable = true;
+
+      layout = "gb";
+
+      videoDrivers = [ "nvidia" ];
+
+      xkbVariant = "";
+    };
+  };
+
+  security.rtkit.enable = true;
+
+  sound.enable = true;
+
+  system.stateVersion = "23.11";
+
+  time.timeZone = "Europe/London";
+
+  users.users.adamc = {
+    description = "Adam Chance";
+    extraGroups = [ "networkmanager" "libvirtd" "wheel" ];
+    isNormalUser = true;
+  };
+}
