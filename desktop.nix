a0840b5a24154b8fea6fd8196b51dea137811898
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-03-07 22:05:04 +0000

    Remove live and RPi images, and cleanup

--- desktop.nix
+++ desktop.nix
@@ -3,23 +3,52 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./desktop/configuration.nix
-    ./desktop/unstable.nix
+    ./unstable.nix
   ];
 
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    systemd-boot.enable = true;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      efi.canTouchEfiVariables = true;
+
+      systemd-boot.enable = true;
+    };
+
+    supportedFilesystems = [ "bcachefs" ];
   };
 
+  console.keyMap = "uk";
+
+  documentation.nixos.enable = false;
+
   environment = {
+    gnome.excludePackages = with pkgs; [
+      epiphany
+      gnome.geary
+      gnome.gnome-calendar
+      gnome.gnome-characters
+      gnome.gnome-clocks
+      gnome.gnome-contacts
+      gnome.gnome-font-viewer
+      gnome.gnome-maps
+      gnome.gnome-music
+      gnome.simple-scan
+      gnome-connections
+      gnome-tour
+      gnome.yelp
+      snapshot
+    ];
+
     sessionVariables.NIXOS_OZONE_WL = "1";
 
     systemPackages = with pkgs; [
       discord
       ffmpeg
+      firefox
       jetbrains.idea-ultimate
       # r2modman
+      vim
       # webcord
       xivlauncher
 
@@ -37,7 +66,45 @@
     ];
   };
 
-  hardware.opengl.driSupport32Bit = true;
+  hardware = {
+    nvidia.modesetting.enable = true;
+
+    opengl.driSupport32Bit = true;
+
+    pulseaudio.enable = false;
+  };
+
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+  };
+
+  networking = {
+    hostName = "nixos";
+
+    networkmanager.enable = true;
+  };
+
+  nixpkgs = {
+    config.allowUnfree = true;
+
+    overlays = [(final: super: {
+      zfs = super.zfs.overrideAttrs(_: {
+        meta.platforms = [];
+      });
+    })];
+  };
 
   programs = {
     _1password-gui.enable = true;
@@ -45,18 +112,54 @@
     gamemode = {
       enable = true;
 
-      settings.general = {
-        renice = 20;
-      };
+      settings.general.renice = 20;
     };
 
     steam = {
       enable = true;
+
       gamescopeSession.enable = true;
     };
   };
 
-  # services.openssh.enable = true;
+  services = {
+    openssh.enable = false;
+
+    pipewire = {
+      enable = true;
+
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+
+      pulse.enable = true;
+    };
+
+    xserver = {
+      enable = true;
+
+      excludePackages = [ pkgs.xterm ];
+
+      displayManager.gdm.enable = true;
+
+      desktopManager.gnome.enable = true;
+
+      layout = "gb";
+
+      videoDrivers = [ "nvidia" ];
+
+      xkbVariant = "";
+    };
+  };
+
+  security.rtkit.enable = true;
+
+  sound.enable = true;
+
+  system.stateVersion = "23.11";
+
+  time.timeZone = "Europe/London";
 
   users.users.adamc = {
     description = "Adam Chance";
