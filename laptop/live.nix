0e9f289415abb90d6a0d2b58f649b3b08a4f09f1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-03-09 20:09:19 +0000

    Re-arrange files

--- laptop/live.nix
+++ laptop/live.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
-    ../laptop.nix
+    ./configuration.nix
   ];
 
   isoImage.squashfsCompression = "zstd";
