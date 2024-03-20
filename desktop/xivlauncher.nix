66e586b7e75c26a63a5d326911b976610692d287
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-03-20 17:39:44 +0000

    Fix HTTPS error

--- desktop/xivlauncher.nix
+++ desktop/xivlauncher.nix
@@ -2,7 +2,7 @@
 
 let
   release = import
-    (builtins.fetchTarball hhttps://github.com/NixOS/nixpkgs/archive/8a26e1beb022d7e9a22d86cbbe1c444b68ebe4a4.tar.gz)
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/8a26e1beb022d7e9a22d86cbbe1c444b68ebe4a4.tar.gz)
     { config = config.nixpkgs.config; };
 in
 {
