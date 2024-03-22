8b5aeec373e14c7eb3d615c78e92304a91ba5204
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-03-22 20:08:34 +0000

    Whitespace

--- desktop/xivlauncher.nix
+++ desktop/xivlauncher.nix
@@ -4,8 +4,7 @@ let
   tz = import
     (builtins.fetchTarball https://github.com/witchof0x20/nixpkgs/archive/931562ee7b06547aa76838dada36cacc71b3b61a.tar.gz)
     { config = config.nixpkgs.config; };
-in
-{
+in {
   environment.systemPackages = with pkgs; [
     tz.xivlauncher
   ];
