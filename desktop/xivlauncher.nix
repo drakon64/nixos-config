689a2cc75ddb12e30e4a1d67a7d5d86a19c0deb0
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-03-30 19:39:47 +0000

    Use XIVLauncher-RB with Wayland

--- desktop/xivlauncher.nix
+++ desktop/xivlauncher.nix
@@ -2,12 +2,13 @@
 
 let
   rb = import
-    (builtins.fetchTarball https://github.com/drakon64/nixpkgs/archive/bf514a1ea654811b459818a8a1a1714848dbde78.tar.gz)
+    (builtins.fetchTarball https://github.com/drakon64/nixpkgs/archive/da3468b89081a0192d2f9b6ef2cea53e95375384.tar.gz)
     { config = config.nixpkgs.config; };
 in {
   environment.systemPackages = with pkgs; [
     (rb.xivlauncher.override {
       useGameMode = true;
+      useRbPatchedLauncher = true;
     })
   ];
 }
