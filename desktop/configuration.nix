d20d3593ff3d9208d3c3574564b4812265ef97aa
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-05 17:51:19 +0100

    Prep for COSMIC desktop

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -153,6 +153,10 @@
   };
 
   services = {
+    # desktopManager.cosmic.enable = true;
+
+    # displayManager.cosmic-greeter.enable = true;
+
     flatpak.enable = true;
 
     fwupd.enable = true;
