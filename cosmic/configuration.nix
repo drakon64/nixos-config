51378356c99b711f2ca5e4b94f5e09afd8067685
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-06 15:29:25 +0100

    Attempt building a COSMIC Live ISO

---
+++ cosmic/configuration.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.kernelPackages = pkgs.linuxPackages_6_8;
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+
+      open = true;
+    };
+
+    pulseaudio.enable = lib.mkForce false;
+  };
+
+  networking.hostName = "cosmic";
+
+  nixpkgs = {
+    config.allowUnfree = true;
+
+    hostPlatform = "x86_64-linux";
+  };
+
+  services = {
+    desktopManager.cosmic.enable = true;
+
+    displayManager = {
+      autoLogin = {
+        enable = true;
+
+        user = "nixos";
+      };
+
+      cosmic-greeter.enable = true;
+    };
+
+    xserver.videoDrivers = [ "nvidia" ];
+  };
+
+  system.stateVersion = "24.05";
+}
