1d110074edf41167227bc1971fd84e253cf98cbc
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-06-10 18:47:07 +0100

    Update `xivlauncher-rb` to `1.0.8.3`

--- desktop/xivlauncher.nix
+++ desktop/xivlauncher.nix
@@ -2,7 +2,7 @@
 
 let
   rb = import
-    (builtins.fetchTarball https://github.com/drakon64/nixpkgs/archive/3c52a879f9a6029275d0dbd6ada73c2d0aca239d.tar.gz)
+    (builtins.fetchTarball https://github.com/drakon64/nixpkgs/archive/0b5d3ff0b6da3d3032cab1a4a0ebf97439ccbcd9.tar.gz)
     { config = config.nixpkgs.config; };
 in {
   environment.systemPackages = with pkgs; [
