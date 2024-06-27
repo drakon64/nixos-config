59c775c066692f9987a4a2178e4238a1ea7efaec
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-06-27 22:07:01 +0100

    Switch back to 'upstream' XIVLauncher

--- desktop/xivlauncher.nix
+++ desktop/xivlauncher.nix
@@ -2,13 +2,13 @@
 
 let
   rb = import
-    (builtins.fetchTarball https://github.com/drakon64/nixpkgs/archive/0b5d3ff0b6da3d3032cab1a4a0ebf97439ccbcd9.tar.gz)
+    (builtins.fetchTarball https://github.com/sersorrel/nixpkgs/archive/86555373894cba05cdbbb274d9b412b87e76ad98.tar.gz)
     { config = config.nixpkgs.config; };
 in {
   environment.systemPackages = with pkgs; [
     (rb.xivlauncher.override {
-      useGameMode = true;
-      useRbPatchedLauncher = true;
+#      useGameMode = true;
+#      useRbPatchedLauncher = true;
     })
   ];
 }
