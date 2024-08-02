65e19e06b6c8cbf73b13e5085baf414b4efb4448
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-02 22:45:22 +0100

    Move CachyOS kernel to its own file

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -5,12 +5,11 @@
   ...
 }:
 
-let
-  kernelVersion = "6.10";
-  cachyRev = "f036a67118ed302c3611e82e0234f8d4279079af";
-in
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [
+    ./hardware-configuration.nix
+    ./cachyos.nix
+  ];
 
   boot = {
     kernelPackages = pkgs.linuxPackages_6_10;
@@ -19,76 +18,6 @@ in
       "amd_pstate=active"
       "initcall_blacklist=acpi_cpufreq_init"
       "nvidia_drm.fbdev=1"
-    ];
-
-    # https://github.com/NixOS/nixpkgs/pull/330917
-    kernelPatches = [
-      {
-        name = "cachyos-base-all";
-        patch = (
-          pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/${cachyRev}/${kernelVersion}/all/0001-cachyos-base-all.patch";
-            hash = "sha256-lZbElRTvhc6u6rEQAbpdRSPLUj/rPTqKX1ZFdMTMZlo=";
-          }
-        );
-
-        extraStructuredConfig = with lib.kernel; {
-          GENERIC_CPU = no;
-          MZEN3 = yes;
-
-          CACHY = yes;
-
-          LTO_NONE = yes;
-
-          HZ_300 = no;
-          HZ_1000 = yes;
-          HZ = freeform "1000";
-
-          NR_CPUS = lib.mkForce (freeform "16");
-
-          HZ_PERIODIC = no;
-          NO_HZ_IDLE = no;
-          NO_HZ_FULL = yes;
-          NO_HZ = yes;
-          NO_HZ_COMMON = yes;
-          CONTEXT_TRACKING = yes;
-
-          PREEMPT_BUILD = yes;
-          PREEMPT_NONE = no;
-          PREEMPT_VOLUNTARY = lib.mkForce no;
-          PREEMPT = lib.mkForce yes;
-          PREEMPT_COUNT = yes;
-          PREEMPTION = yes;
-          PREEMPT_DYNAMIC = yes;
-
-          CC_OPTIMIZE_FOR_PERFORMANCE = no;
-          CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
-
-          TCP_CONG_CUBIC = lib.mkForce module;
-          TCP_CONG_BBR = yes;
-          DEFAULT_BBR = yes;
-          DEFAULT_TCP_CONG = freeform "bbr";
-
-          TRANSPARENT_HUGEPAGE_MADVISE = lib.mkForce no;
-          TRANSPARENT_HUGEPAGE_ALWAYS = lib.mkForce yes;
-
-          USER_NS = yes;
-        };
-      }
-      {
-        name = "bore-cachy";
-        patch = (
-          pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/${cachyRev}/${kernelVersion}/sched/0001-bore-cachy.patch";
-            hash = "sha256-qSRPWTMsRigTQpaEWP6shrlIfkaeRz/Lm/XgWYBQwbE=";
-          }
-        );
-
-        extraStructuredConfig = with lib.kernel; {
-          SCHED_BORE = yes;
-          MIN_BASE_SLICE_NS = freeform "1000000";
-        };
-      }
     ];
 
     loader = {
