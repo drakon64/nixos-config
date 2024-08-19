469b1afff0d5f3f5f8c9e0c4152f8e1d80721d7f
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-19 19:22:15 +0100

    Manage DLSSTweaks INI and DLSS DLL's with Home Manager

--- desktop/home.nix
+++ desktop/home.nix
@@ -12,6 +12,17 @@ let
     sha256 = "19w5kjgy5zayp02mz5rv5g91r31ix5rm1m95qwlxfrp9gbag9dh2";
     stripRoot = false;
   };
+
+  dlssTweaksIni = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/drakon64/DLSSTweaks/25516be262643d74ede0da5794c8687fc1f6546b/dlsstweaks.ini";
+    sha256 = "04r04lgkxngdcrvvbcka3pbq82w4q1b09dw6xpdgbg4i46yafmvs";
+  };
+
+  dlss = pkgs.fetchzip {
+    url = "https://developer.nvidia.com/downloads/assets/gameworks/downloads/secure/dlss/dlss_3_7_10.zip";
+    sha256 = "037gza9pwb55xqgq2wnns5kfrki03whiyg7bqxlcnsqbf7grrjmr";
+    stripRoot = true;
+  };
 in
 {
   home = {
@@ -27,6 +38,10 @@ in
 
     # DLSSTweaks
     file.".xlcore/ffxiv/game/winmm.dll".source = "${dlssTweaks}/nvngx.dll";
+    file.".xlcore/ffxiv/game/dlsstweaks.ini".source = dlssTweaksIni;
+
+    # DLSS
+    file.".xlcore/ffxiv/game/nvngx_dlss_new.dll".source = "${dlss}/lib/Windows_x86_64/rel/nvngx_dlss.dll";
   };
 
   programs.home-manager.enable = true;
