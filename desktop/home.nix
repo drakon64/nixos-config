95c4775b2f9fb730fbacaccc12bdb8f58cf081dc
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-09-02 21:29:08 +0100

    Remove XIVLauncher DLSS hacks

--- desktop/home.nix
+++ desktop/home.nix
@@ -1,12 +1,6 @@
-{ pkgs, osConfig, ... }:
+{ pkgs, ... }:
 
 let
-  dxvk-nvapi = pkgs.fetchzip {
-    url = "https://github.com/jp7677/dxvk-nvapi/releases/download/v0.7.1/dxvk-nvapi-v0.7.1.tar.gz";
-    sha256 = "076sjchv308zc7fv6mrdph2kd8w5nvgrqjc4acmgpwj3s7v85yjv";
-    stripRoot = false;
-  };
-
   dlssTweaks = pkgs.fetchzip {
     url = "https://github.com/emoose/DLSSTweaks/files/15013420/DLSSTweaks-0.200.9.0-beta1.zip";
     sha256 = "19w5kjgy5zayp02mz5rv5g91r31ix5rm1m95qwlxfrp9gbag9dh2";
@@ -31,11 +25,6 @@ in
     username = "adamc";
 
     file = {
-      # DXVK-NVAPI
-      ".xlcore/wineprefix/drive_c/windows/system32/nvapi64.dll".source = "${dxvk-nvapi}/x64/nvapi64.dll";
-      ".xlcore/ffxiv/game/nvngx.dll".source = "${osConfig.hardware.nvidia.package}/lib/nvidia/wine/nvngx.dll";
-      ".xlcore/ffxiv/game/_nvngx.dll".source = "${osConfig.hardware.nvidia.package}/lib/nvidia/wine/_nvngx.dll";
-
       # DLSSTweaks
       ".xlcore/ffxiv/game/winmm.dll".source = "${dlssTweaks}/nvngx.dll";
       ".xlcore/ffxiv/game/dlsstweaks.ini".source = dlssTweaksIni;
