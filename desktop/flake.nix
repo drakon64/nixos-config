254e5ff18f89bcc03ba9e856a4baeacd5794f357
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-01 16:36:02 +0000

    Desktop: Rewrite for NixOS 24.11

--- desktop/flake.nix
+++ desktop/flake.nix
@@ -2,23 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-xivlauncher-rb = {
       url = "github:drakon64/nixos-xivlauncher-rb";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-cosmic = {
+      url = "github:drakon64/nixos-cosmic";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -27,26 +27,25 @@
     inputs@{
       self,
       nixpkgs,
-      home-manager,
       lix-module,
-      nixos-cosmic,
+      home-manager,
       nixos-xivlauncher-rb,
+      nixos-cosmic,
     }:
     {
       nixosConfigurations = {
         desktop = nixpkgs.lib.nixosSystem {
           modules = [
+            lix-module.nixosModules.default
+            nixos-cosmic.nixosModules.default
             ./configuration.nix
-
             home-manager.nixosModules.home-manager
+
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.adamc = import ./home.nix;
             }
-
-            lix-module.nixosModules.default
-            nixos-cosmic.nixosModules.default
           ];
 
           specialArgs = {
--- desktop/home.nix
+++ desktop/home.nix
@@ -17,13 +17,12 @@ let
     sha256 = "037gza9pwb55xqgq2wnns5kfrki03whiyg7bqxlcnsqbf7grrjmr";
     stripRoot = true;
   };
+
+  cosmic-wallpapers = "${pkgs.cosmic-wallpapers}/share/backgrounds/cosmic";
+  wallpapers = "Pictures/Wallpapers";
 in
 {
   home = {
-    homeDirectory = "/home/adamc";
-    stateVersion = "24.05";
-    username = "adamc";
-
     file = {
       # DLSSTweaks
       ".xlcore/ffxiv/game/winmm.dll".source = "${dlssTweaks}/nvngx.dll";
@@ -31,8 +30,49 @@ in
 
       # DLSS
       ".xlcore/ffxiv/game/nvngx_dlss_new.dll".source = "${dlss}/lib/Windows_x86_64/rel/nvngx_dlss.dll";
+
+      # Wallpapers
+      "${wallpapers}/A_stormy_stellar_nursery_esa_379309.jpg".source = "${cosmic-wallpapers}/A_stormy_stellar_nursery_esa_379309.jpg";
+      "${wallpapers}/orion_nebula_nasa_heic0601a.jpg".source = "${cosmic-wallpapers}/orion_nebula_nasa_heic0601a.jpg";
+      "${wallpapers}/otherworldly_earth_nasa_ISS064-E-29444.jpg".source = "${cosmic-wallpapers}/otherworldly_earth_nasa_ISS064-E-29444.jpg";
+      "${wallpapers}/tarantula_nebula_nasa_PIA23646.jpg".source = "${cosmic-wallpapers}/tarantula_nebula_nasa_PIA23646.jpg";
     };
+
+    homeDirectory = "/home/adamc";
+    stateVersion = "24.11";
+    username = "adamc";
   };
 
   programs.home-manager.enable = true;
+
+  dconf = {
+    enable = true;
+
+    settings."org/gnome/shell" = {
+      disable-user-extensions = false;
+
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        alphabetical-app-grid.extensionUuid
+        appindicator.extensionUuid
+        dash-to-dock.extensionUuid
+      ];
+    };
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "6444703+drakon64@users.noreply.github.com";
+      userName = "Adam Chance";
+    };
+
+    ssh = {
+      enable = true;
+
+      #extraConfig = ''
+      #  Host *
+      #    IdentityAgent ~/.1password/agent.sock
+      #'';
+    };
+  };
 }
