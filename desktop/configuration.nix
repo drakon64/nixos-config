984c1aa153e76ba620e7e71ddf8fe7a6e35d7f5a
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-20 23:14:00 +0000

    Desktop: Shortcut for UEFI virtualisation

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -55,6 +55,11 @@
       jetbrains.idea-community
 
       (pkgs.callPackage ./krisp-patcher.nix { })
+      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
 
       (wrapOBS {
         plugins = with obs-studio-plugins; [
