f9af827a0d6cb94e92251e32a8fe90b925a81c23
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-21 15:28:20 +0000

    Desktop: Remove QEMU UEFI shortcut

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -58,11 +58,6 @@
       jetbrains.idea-community
 
       (pkgs.callPackage ./krisp-patcher.nix { })
-      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
-        qemu-system-x86_64 \
-          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-          "$@"
-      '')
 
       (wrapOBS {
         plugins = with obs-studio-plugins; [
