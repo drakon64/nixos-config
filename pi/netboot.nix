178a07433961d1448e3ef9885b233cbfa8325716
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-21 01:17:23 +0000

    Pi: Netboot Bcachefs support

--- pi/netboot.nix
+++ pi/netboot.nix
@@ -1,6 +1,20 @@
-{ lib, modulesPath, ... }:
+{
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 {
   imports = [ "${modulesPath}/installer/netboot/netboot.nix" ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    supportedFilesystems = {
+      bcachefs = true;
+      zfs = lib.mkForce false;
+    };
+  };
 
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "24.11";
