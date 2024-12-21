a6255ae076eb7b570a1d1ab9545b492399a02a5b
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-21 15:24:50 +0000

    Pi: Simplify config

--- pi/flake.nix
+++ pi/flake.nix
@@ -10,36 +10,37 @@
       nixpkgs,
       nixos-hardware,
     }:
-    let
-      ipxe =
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in
-        pkgs.ipxe.override {
-          embedScript = pkgs.writeText "embed" ''
-            #!ipxe
-
-            dhcp
-            chain http://192.168.2.2/netboot.ipxe
-          '';
-        };
-    in
     {
       nixosConfigurations = rec {
         netboot = nixpkgs.lib.nixosSystem {
           modules = [ ./netboot.nix ];
         };
 
-        pi = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./configuration.nix
-            nixos-hardware.nixosModules.raspberry-pi-3
-          ];
+        pi =
+          let
+            ipxe =
+              let
+                pkgs = nixpkgs.legacyPackages.x86_64-linux;
+              in
+              pkgs.ipxe.override {
+                embedScript = pkgs.writeText "embed" ''
+                  #!ipxe
 
-          specialArgs = {
-            inherit ipxe netboot;
+                  dhcp
+                  chain http://192.168.2.2/netboot.ipxe
+                '';
+              };
+          in
+          nixpkgs.lib.nixosSystem {
+            modules = [
+              ./configuration.nix
+              nixos-hardware.nixosModules.raspberry-pi-3
+            ];
+
+            specialArgs = {
+              inherit ipxe netboot;
+            };
           };
-        };
       };
     };
 }
