6225640a822a8fc1bfa33436f064df7b9c45ce1c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-01-02 19:16:40 +0000

    Desktop: Include script for starting UEFI VM's

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -65,6 +65,12 @@
           obs-vkcapture
         ];
       })
+
+      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
     ];
   };
 
