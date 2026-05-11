ed6c98dbecf0e3b91231db4c29b48bff98e4fd2e
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-05-11 09:34:33 +0100

    Begin packaging netboot images

--- pi/ipxe.nix
+++ pi/ipxe.nix
@@ -6,15 +6,28 @@
 }:
 
 let
-  #  ipxeScript = pkgs.writeText "boot.ipxe" ''
-  #    #!ipxe
-  #    kernel http://pi.drakon.local/kernel init=init initrd=initrd ${toString config.boot.kernelParams}
-  #    initrd http://pi.drakon.local/initrd
-  #    boot
-  #  '';
+  #    ipxeScript = pkgs.writeText "boot.ipxe" ''
+  #      #!ipxe
+  #      kernel http://pi.drakon.local/kernel init=init initrd=initrd ${toString config.boot.kernelParams}
+  #      initrd http://pi.drakon.local/initrd
+  #      boot
+  #    '';
 
-  # TODO: Reuse `pkgs` rather than reimporting
-  pkgs86 = import (import ../lon.nix).nixpkgs { system = "x86_64-linux"; };
+  netboot =
+    let
+      configEvaled =
+        let
+          # TODO: Reuse `pkgs` rather than reimporting
+          pkgs86 = import (import ../lon.nix).nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs86.nixos [ ./netboot.nix ];
+
+      build = configEvaled.config.system.build;
+    in
+    {
+      ramdisk = build.netbootRamdisk + "initrd";
+      kernel = build.kernel + configEvaled.pkgs.stdenv.hostPlatform.linux-kernel.target;
+    };
 in
 {
   boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
@@ -24,6 +37,13 @@ in
     #      embedScript = ipxeScript;
     #    })
 
-    (pkgs86.nixos [ ./netboot.nix ]).config.system.build.netbootRamdisk
+    (pkgs.symlinkJoin {
+      name = "netboot";
+
+      paths = [
+        netboot.kernel
+        netboot.ramdisk
+      ];
+    })
   ];
 }
