5acade3231baefe8f245f7e311056218f56368c5
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-05-12 11:53:54 +0100

    Build netboot images with GitHub Actions

--- default.nix
+++ default.nix
@@ -2,5 +2,22 @@ let
   pkgs = import (import ./lon.nix).nixpkgs { };
 in
 {
+  netboot =
+    let
+      configEvaled = pkgs.nixos [ ./netboot/configuration.nix ];
+    in
+    {
+      kernel = configEvaled.config.system.build.kernel;
+      initrd = configEvaled.config.system.build.netbootRamdisk;
+
+      ipxe = pkgs.writeText "boot.ipxe" ''
+        #!ipxe
+
+        kernel bzImage init=${configEvaled.config.system.build.toplevel}/init initrd=initrd ${toString configEvaled.config.boot.kernelParams}
+        initrd initrd
+        boot
+      '';
+    };
+
   pi = (pkgs.nixos [ ./pi/configuration.nix ]).config.system.build.sdImage;
 }
