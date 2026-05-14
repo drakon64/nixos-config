3594c2a1464bbe0a1e79a32d6afafbf751809d60
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-05-14 13:22:17 +0100

    Remove Pi and Netboot

--- shell.nix
+++ shell.nix
@@ -3,14 +3,7 @@ let
 in
 pkgs.mkShellNoCC {
   packages = with pkgs; [
-    colmena
     lon
     nixfmt
-    opentofu
-    zstd
   ];
-
-  passthru.opentofu = pkgs.mkShellNoCC {
-    packages = [ pkgs.opentofu ];
-  };
 }
